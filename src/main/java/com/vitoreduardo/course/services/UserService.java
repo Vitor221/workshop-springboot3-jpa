1f9b4e6878d68dbb7f0175d57e9d0960b9ab7b40
Vitor <vitornascimento6650@gmail.com>
2023-11-23 12:06:11 -0300

    Service layer, component registration

---
+++ src/main/java/com/vitoreduardo/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.vitoreduardo.course.services;
+
+import com.vitoreduardo.course.entities.User;
+import com.vitoreduardo.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findByid(Long id) {
+        Optional<User> user = repository.findById(id);
+        return user.get();
+    }
+}
